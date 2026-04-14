02655fb7d1be9018ef67f39c9d8e31546a4dfe86
Akshat-tyagi <akshattyagi146@gmail.com>
2026-04-14 13:45:02 +0530

    added initial part

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import prisma from "./db/prisma.js";
+
+const app = express();
+
+app.get("/",async(req,res)=>{
+    const users = await prisma.user.findMany();
+    res.json({users});
+})
+
+app.post("/",async(req,res)=>{
+    await prisma.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    });
+    res.json({message:"created successfully"})
+})
+
+app.listen(3000);
